e42e6723ebef41f4cd13c7bdf414dc96cfeb7979
Namemark <you@example.com>
2024-04-23 21:37:04 +0300

    creation new

---
+++ Week  2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+Console.WriteLine("Enter your age: ");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isageNumber = int.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isageNumber}. User is {userAgeNum} years old");
+
+if (isageNumber)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram.");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age");
+}
